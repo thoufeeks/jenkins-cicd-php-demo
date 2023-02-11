eef685b040a3b1329d51654f5858aa4c43ea381f
Thoufeek <34390489+thoufeeks@users.noreply.github.com>
2023-02-12 01:28:33 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>Index</title>
  </head>
  <body>
- <?php echo '<p>Hello world from PHP! sdsDeclarative 212pipeline!!</p>'; ?> 
+ <?php echo '<p>Hello world from PHsdP! sdsDeclarative 212pipeline!!</p>'; ?> 
  </body>
 </html>
